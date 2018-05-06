7ada5970c458257155abdc5045cba717503d296a
samdd <sam@samdd.me>
2018-05-06 16:14:00 +0100

    Disable development debugger

--- src/controllers/cerebral/index.ts
+++ src/controllers/cerebral/index.ts
@@ -3,8 +3,11 @@ import Devtools from 'cerebral/devtools'
 
 import * as app from '../../store'
 
+const development =
+  process.env.NODE_ENV === 'development' || /localhost/.test(location.host)
+
 const controller = Controller(app.module, {
-  devtools: Devtools({ host: 'localhost:9000' }) // process.env.NODE_ENV
+  ...(development ? { devtools: Devtools({ host: 'localhost:9000' }) } : {})
 })
 
 export default controller
