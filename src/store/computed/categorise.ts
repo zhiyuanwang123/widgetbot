3b9458c07a0de712b325c69457fb8c91a7fe5187
samdd <sam@samdd.me>
2018-05-16 14:58:57 +0100

    Fix

--- src/store/computed/categorise.ts
+++ src/store/computed/categorise.ts
@@ -12,17 +12,25 @@ const Categorise = (channels: Channels): Category[] => {
     }
 
     if (channel.category) {
+      // The channel belongs in a named category
       let index = indexes.get(channel.category)
+
+      // If the category already exists
       if (typeof index === 'number') {
+        // Push the channel
         categorised[index].channels.push(channel)
       } else {
+        // Create a new category
         index = categorised.push(newCategory) - 1
         indexes.set(channel.category, index)
       }
     } else {
+      // The channel doesn't belong in a named category
       const lastCategory = categorised[categorised.length - 1]
 
-      if (!lastCategory.name) {
+      // If the last category was unamed, then insert
+      // this channel into it
+      if (lastCategory && !lastCategory.name) {
         lastCategory.channels.push(channel)
       } else {
         categorised.push(newCategory)
