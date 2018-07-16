9fc9ba603e6e4cf175042525224b40976d25d8dc
Sam Denty <samddenty@gmail.com>
2018-07-16 02:27:00 +0100

    Check before using webp

--- src/components/shared/ExpandableImage/optimize.ts
+++ src/components/shared/ExpandableImage/optimize.ts
@@ -1,4 +1,14 @@
+import memoize from 'memoizee'
+
 const DISCORD_CDN = /https:\/\/cdn.discordapp.com\/attachments\/(\d+)\/(\d+)\/(.+)\.(png|gif)/
+
+const canUseWebP = memoize(() => {
+  const elem = document.createElement('canvas')
+  if (!!(elem.getContext && elem.getContext('2d'))) {
+    return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0
+  }
+  return false
+})
 
 const optimize = ({ width, height, url }) => {
   const match = url.match(DISCORD_CDN)
@@ -7,7 +17,7 @@ const optimize = ({ width, height, url }) => {
     const [, id1, id2, file, type] = match
 
     return `https://media.discordapp.net/attachments/${id1}/${id2}/${file}.${type}?height=${height}&width=${width}${
-      type === 'png' ? '&format=webp' : ''
+      type === 'png' && canUseWebP() ? '&format=webp' : ''
     }`
   }
 
