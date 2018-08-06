83b85e173274733b8d8f8982cd700b1b78c428b4
Sam Denty <samddenty+gh@gmail.com>
2018-08-06 17:15:28 +0100

    More translatables

--- src/views/ChooseChannel/index.tsx
+++ src/views/ChooseChannel/index.tsx
@@ -3,12 +3,19 @@ import * as React from 'react'
 import { Chat } from '@ui/Overlays'
 import Header, { Name, Topic } from '@ui/Header'
 import Wrapper from '@ui/Wrapper'
+import { Trans } from '@lingui/react'
 
 const ChooseChannel = () => (
   <Wrapper>
     <Header>
-      <Name>Select a channel</Name>
-      <Topic>Pick a channel from the left</Topic>
+      <Name>
+        <Trans id="PickChannelScreen.Title">Select a channel</Trans>
+      </Name>
+      <Topic>
+        <Trans id="PickChannelScreen.Description">
+          Pick a channel from the left
+        </Trans>
+      </Topic>
     </Header>
     <Chat />
   </Wrapper>
