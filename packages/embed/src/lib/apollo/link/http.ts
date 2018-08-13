8c26fa43e2f84dbe7089d9e77a6d4a4065e4f0c9
Sam Denty <samddenty+gh@gmail.com>
2018-08-13 02:03:12 +0100

    Disable crunching

--- packages/embed/src/lib/apollo/link/http.ts
+++ packages/embed/src/lib/apollo/link/http.ts
@@ -3,7 +3,7 @@ import { BatchHttpLink } from 'apollo-link-batch-http'
 import { onError } from 'apollo-link-error'
 import { uncrunch } from 'graphql-crunch'
 
-const DEVELOPMENT = process.env.NODE_ENV === 'development'
+const CRUNCH = false
 
 const uncruncher = new ApolloLink((operation, forward) =>
   forward(operation).map(response => {
@@ -12,23 +12,22 @@ const uncruncher = new ApolloLink((operation, forward) =>
   })
 )
 
-const httpLink = ApolloLink.from(
-  [
-    !DEVELOPMENT && uncruncher,
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.map(({ message, locations, path }) =>
-          console.error(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-          )
+const httpLink = ApolloLink.from([
+  onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+      graphQLErrors.map(({ message, locations, path }) =>
+        console.error(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
         )
-      if (networkError) console.error(`[Network error]: ${networkError}`)
-    }),
-    new BatchHttpLink({
-      uri: `/api/graphql${DEVELOPMENT ? '' : '?crunch'}`,
-      batchInterval: 20
-    })
-  ].filter(Boolean)
-)
+      )
+    if (networkError) console.error(`[Network error]: ${networkError}`)
+  }),
+  CRUNCH && uncruncher,
+  new BatchHttpLink({
+    uri: `/api/graphql${CRUNCH ? '?crunch' : ''}`,
+    batchInterval: 20,
+    batchMax: 2
+  })
+].filter(Boolean) as any)
 
 export default httpLink
